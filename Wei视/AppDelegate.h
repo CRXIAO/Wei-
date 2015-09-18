17714da91ec89aa90180a9bbc5e8413a496f8058
pan <huhanpan@163.com>
2015-09-18 15:01:28 +0800

    ss
    ss

---
+++ Wei视/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wei视
+//
+//  Created by pan on 15/9/18.
+//  Copyright © 2015年 pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
